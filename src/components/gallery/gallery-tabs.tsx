9d546da8adf522db942858df55282f9765a54535
Wilfred Ekow Abruquah <fredabruquah@gmail.com>
2024-11-11 22:44:32 +0000

    gallery tabs

--- src/components/gallery/gallery-tabs.tsx
+++ src/components/gallery/gallery-tabs.tsx
@@ -3,7 +3,6 @@ import SubTitle from "../sub-title";
 import { Tabs } from "@mantine/core";
 import { body } from "@/lib/fonts";
 import Gallery from "./gallery";
-// import { photos, gallery } from "@/lib/data/gallery";
 import { FC } from "react";
 
 type EventGallery = {
@@ -13,9 +12,15 @@ type EventGallery = {
 
 type GalleryTabsProps = {
   gallery: EventGallery[];
+  agm: EventGallery[];
+  launch: EventGallery[];
+  walk: EventGallery[];
+  screening: EventGallery[];
+  advocacy: EventGallery[];
+  outreaches: EventGallery[];
 };
 
-const GalleryTabs: FC<GalleryTabsProps> = ({ gallery }) => {
+const GalleryTabs: FC<GalleryTabsProps> = ({ gallery, agm, launch, walk, screening, advocacy, outreaches }) => {
   const allImages = gallery.reduce(
     (result: string[], current: EventGallery) => {
       return [...result, ...current.images];
@@ -23,21 +28,74 @@ const GalleryTabs: FC<GalleryTabsProps> = ({ gallery }) => {
     []
   );
 
-  const tabTitles = gallery.map((gallery) => gallery.name);
+  const agmImages = agm.reduce(
+    (result: string[], current: EventGallery) => {
+      return [...result, ...current.images];
+    },
+    []
+  );
 
+
+  const launchImages = launch.reduce(
+    (result: string[], current: EventGallery) => {
+      return [...result, ...current.images];
+    },
+    []
+  );
+
+  const walkImages = walk.reduce(
+    (result: string[], current: EventGallery) => {
+      return [...result, ...current.images];
+    },
+    []
+  );
+
+  const screeningImages = screening.reduce(
+    (result: string[], current: EventGallery) => {
+      return [...result, ...current.images];
+    },
+    []
+  );
+
+  const advocacyImages = advocacy.reduce(
+    (result: string[], current: EventGallery) => {
+      return [...result, ...current.images];
+    },
+    []
+  );
+
+  const outreachesImages = outreaches.reduce(
+    (result: string[], current: EventGallery) => {
+      return [...result, ...current.images];
+    },
+    []
+  );
+
+  const tabTitles = gallery.map((gallery) => gallery.name);
+  
   const shuffleImages = (images: string[]) => {
     for (let i = images.length - 1; i > 0; i--) {
       let j = Math.floor(Math.random() * (i + 1));
       let k = images[i];
-
       images[i] = images[j];
       images[j] = k;
     }
-
     return images;
   };
 
-  const shuffledimages = shuffleImages(allImages);
+  const shuffledImages = shuffleImages(allImages);
+
+  const shuffledAgmImages = shuffleImages(agmImages);
+
+  const shuffledLaunchImages = shuffleImages(launchImages);
+
+  const shuffledWalkImages = shuffleImages(walkImages);
+
+  const shuffledScreeningImages = shuffleImages(screeningImages);
+
+  const shuffledAdvocacyImages = shuffleImages(advocacyImages);
+
+  const shuffledOutreachesImages = shuffleImages(outreachesImages);
 
   return (
     <Container>
@@ -57,29 +115,63 @@ const GalleryTabs: FC<GalleryTabsProps> = ({ gallery }) => {
         >
           <Tabs.List position="center" grow>
             <Tabs.Tab value="All">All Images</Tabs.Tab>
+            <Tabs.Tab value="Some">AGM Images</Tabs.Tab>
+            <Tabs.Tab value="Launch">Launch Images</Tabs.Tab>
+            <Tabs.Tab value="Walk">Walk Images</Tabs.Tab>
+            <Tabs.Tab value="Screening">Screening Images</Tabs.Tab>
+            <Tabs.Tab value="Advocacy">Advocacy Images</Tabs.Tab>
+            <Tabs.Tab value="Outreaches">Outreaches Images</Tabs.Tab>
 
             {tabTitles.length > 2 &&
-              tabTitles.map((title, idx) => {
-                return (
-                  <Tabs.Tab key={idx} value={title}>
-                    <p className="capitalize">{title}</p>
-                  </Tabs.Tab>
-                );
-              })}
+              tabTitles.map((title, idx) => (
+                <Tabs.Tab key={idx} value={title}>
+                  <p className="capitalize">{title}</p>
+                </Tabs.Tab>
+              ))}
           </Tabs.List>
 
+          {/* Panel for "All Images" */}
           <Tabs.Panel value="All" pt="xs">
-            <Gallery images={shuffledimages} />
+            <Gallery images={shuffledImages} />
           </Tabs.Panel>
 
+          {/* Panel for "AGM Images" */}
+          <Tabs.Panel value="Some" pt="xs">
+            <Gallery images={shuffledAgmImages} />
+          </Tabs.Panel>
+
+          {/* Panel for "Launch Images" */}
+          <Tabs.Panel value="Launch" pt="xs">
+            <Gallery images={shuffledLaunchImages} />
+          </Tabs.Panel>
+
+          {/* Panel for "Walk Images" */}
+          <Tabs.Panel value="Walk" pt="xs">
+            <Gallery images={shuffledWalkImages} />
+          </Tabs.Panel>
+
+          {/* Panel for "Screening Images" */}
+          <Tabs.Panel value="Screening" pt="xs">
+            <Gallery images={shuffledScreeningImages} />
+          </Tabs.Panel>
+
+          {/* Panel for "Screening Images" */}
+          <Tabs.Panel value="Advocacy" pt="xs">
+            <Gallery images={shuffledAdvocacyImages} />
+          </Tabs.Panel>
+
+          {/* Panel for "Screening Images" */}
+          <Tabs.Panel value="Outreaches" pt="xs">
+            <Gallery images={shuffledOutreachesImages} />
+          </Tabs.Panel>
+
+          {/* Panels for each event */}
           {gallery.length > 2 &&
-            gallery.map((gallery, idx) => {
-              return (
-                <Tabs.Panel key={idx} value={gallery.name} pt="xs">
-                  <Gallery images={gallery.images} />
-                </Tabs.Panel>
-              );
-            })}
+            gallery.map((gallery, idx) => (
+              <Tabs.Panel key={idx} value={gallery.name} pt="xs">
+                <Gallery images={gallery.images} />
+              </Tabs.Panel>
+            ))}
         </Tabs>
       </div>
     </Container>
--- src/pages/gallery.jsx
+++ src/pages/gallery.jsx
@@ -1,32 +1,131 @@
+// import { PageHero, Tabs } from "@/components";
+// import { client } from "@/lib/contentful";
+
+// const Gallery = ({ eventGallery }) => {
+//   return (
+//     <>
+//       <PageHero title="gallery" />
+//       <Tabs 
+//       gallery={eventGallery}
+//       agm={eventGallery}
+//       launch={eventGallery}
+//       walk={eventGallery}
+//       screening={eventGallery}
+//       advocacy={eventGallery}
+//       outreaches={eventGallery} 
+
+//       />
+//     </>
+//   );
+// };
+
+// export default Gallery;
+
+// export async function getStaticProps() {
+//   const res = await client.getEntries({
+//     content_type: "eventGallery",
+//   });
+
+//   const eventGallery = res.items.map((item) => {
+//     return {
+//       name: item.fields.name,
+//       images: item.fields.images.map((image) => {
+//         return `https:${image.fields.file.url}`;
+//       }),
+//     };
+//   });
+
+//   return {
+//     props: { eventGallery },
+//   };
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import { PageHero, Tabs } from "@/components";
 import { client } from "@/lib/contentful";
 
-const Gallery = ({ eventGallery }) => {
+const Gallery = ({ eventGallery, agm, launch, walk, screening, advocacy, outreaches}) => {
   return (
     <>
       <PageHero title="gallery" />
-      <Tabs gallery={eventGallery} />
+      <Tabs 
+      gallery={eventGallery}
+      agm={agm}
+      launch={launch}
+      walk={walk}
+      screening={screening}
+      advocacy={advocacy}
+      outreaches={outreaches} 
+
+      />
     </>
   );
 };
 
+
 export default Gallery;
 
-export async function getStaticProps() {
-  const res = await client.getEntries({
-    content_type: "eventGallery",
-  });
+// export async function getStaticProps() {
+//   const res = await client.getEntries({
+//     content_type: "eventGallery",
+//   });
 
-  const eventGallery = res.items.map((item) => {
-    return {
+//   const eventGallery = res.items.map((item) => {
+//     return {
+//       name: item.fields.name,
+//       images: item.fields.images.map((image) => {
+//         return `https:${image.fields.file.url}`;
+//       }),
+//     };
+//   });
+
+//   return {
+//     props: { eventGallery },
+//   };
+// }
+
+
+
+
+export async function getStaticProps() {
+  // Helper function to fetch data by content type
+  const fetchContent = async (contentType) => {
+    const res = await client.getEntries({ content_type: contentType });
+    return res.items.map((item) => ({
       name: item.fields.name,
-      images: item.fields.images.map((image) => {
-        return `https:${image.fields.file.url}`;
-      }),
-    };
-  });
+      images: item.fields.images.map((image) => `https:${image.fields.file.url}`)
+    }));
+  };
+
+
+  // Fetch data for each category
+  const [eventGallery, agm, launch, walk, screening, advocacy, outreaches] = await Promise.all([
+    fetchContent("eventGallery"),
+    fetchContent("agm"),
+    fetchContent("launchGallery"),
+    fetchContent("walkGallery"),
+    fetchContent("screeningGallery"),
+    fetchContent("advocacyGallery"),
+    fetchContent("outreachesGallery")
+  ]);
 
   return {
-    props: { eventGallery },
+    props: { eventGallery, agm, launch, walk, screening, advocacy, outreaches},
   };
 }
